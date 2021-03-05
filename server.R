193c554df50716a99c43c2522c6e38ad581c3532
Justin Post <jbpost2@ncsu.edu>
2021-03-05 11:15:10 -0500

    Added second relationship

--- server.R
+++ server.R
@@ -46,4 +46,50 @@ shinyServer(function(input, output) {
     plot(x=x,y=dbeta(x=x,shape1=numsuccess+alphaval,shape2=n-numsuccess+betaval),main=paste("Posterior Density for Theta|Y=",numsuccess,sep=""),xlab="theta's", ylab="f(theta|y)",type="l")
   })
 
+  
+  
+  #Create prior plot output
+  output$priorPlot2 <- renderPlot({
+    
+    #get alpha and beta values from input
+    alphaval<-input$alpha2
+    betaval<-input$beta2
+
+    #set defaults if not supplied
+    if (is.na(alphaval)){alphaval<-1}
+    if (is.na(betaval)){betaval<-1}
+    
+    #Plotting sequence
+    x <- seq(from=0,to=alphaval/betaval+4*sqrt(alphaval/betaval),length = 2000)
+
+        #draw the prior distribution plot
+    plot(x=x,y=dgamma(x=x,shape=alphaval,rate=betaval),main="Prior Density for Lambda",xlab="lambda's", ylab="f(lambda)",type="l")
+    
+  })
+  
+  #create posterior plot  
+  output$distPlot2 <- renderPlot({
+    
+    #get alpha and beta values from input
+    alphaval<-input$alpha2
+    betaval<-input$beta2
+    
+    #set defaults if not supplied
+    if (is.na(alphaval)){alphaval<-1}
+    if (is.na(betaval)){betaval<-1}
+    
+    #number of success from input slider
+    sampMean <- input$sampleMean
+    sampSize <- input$sampleSize
+    
+    newalpha <- alphaval + sampMean*sampSize
+    newbeta <- betaval + sampSize
+
+    #Plotting sequence
+    x <- seq(from=0,to=newalpha/newbeta+4*sqrt(newalpha/newbeta),length = 2000)
+    
+    # draw the posterior
+    plot(x=x,y=dgamma(x=x,shape=newalpha,rate = newbeta),main=paste("Posterior Density for Lambda|y's",sep=""),xlab="lambda's", ylab="f(lambda|y's)",type="l")
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 ###########################################################################
 ##R Shiny App to plot different possible posterior distributions from coin example
-##Justin Post - Fall 2016 (updated 2017)
+##Added Gamma/pois
+##Justin Post 
 ###########################################################################
 
 #Load package
@@ -12,12 +13,13 @@ library(shinydashboard)
 dashboardPage(skin="red",
 
   #add title
-  dashboardHeader(title="Posterior Distribution for Coin Example",titleWidth=1000),
+  dashboardHeader(title="Posterior Distribution Visuals",titleWidth=1000),
   
   #define sidebar items
   dashboardSidebar(sidebarMenu(
     menuItem("About", tabName = "about", icon = icon("archive")),
-    menuItem("Application", tabName = "app", icon = icon("laptop"))
+    menuItem("Binomial & Beta", tabName = "app", icon = icon("laptop")),
+    menuItem("Gamma & Poisson", tabName = "app2", icon = icon("laptop"))
   )),
 
   #define the body of the app
@@ -35,10 +37,14 @@ dashboardPage(skin="red",
             h1("What does this app do?"),
             #box to contain description
             box(background="red",width=12,
-              h4("This application shows the relationship between the prior distribution and the posterior distribution for a simple Bayesian model."),
-              h4("The prior distribution is assumed to be a Beta distribution and the likelihood is a Binomial distribution with 30 trials (of which you can change the number of successes).  This yields a Beta distribution as the posterior. Note: As the prior distribution is in the same family as the posterior, we say the prior is conjugate for the likelihood."),
-              h4("This application corresponds to an example in ",span("Mathematical Statistics and Data Analysis",style = "font-style:italic"), "section 3.5, example E, by John Rice."),
-              h4("The goal of the example is to update our belief about the parameter \\(\\Theta\\) = the probability of obtaining a head when a particular coin is flipped.  The experiment is to flip the coin 30 times and observe the number of heads. The likelihood is then a binomial distribution. The prior is assumed to be a Beta distribution.")
+              h4("This application shows the relationship between the prior distribution and the posterior distribution for two simple Bayesian models."),
+              h4("For the 'Binomial & Beta' app:"),
+              h5("The prior distribution is assumed to be a Beta distribution and the likelihood is a Binomial distribution with 30 trials (of which you can change the number of successes).  This yields a Beta distribution as the posterior. Note: As the prior distribution is in the same family as the posterior, we say the prior is conjugate for the likelihood."),
+              h5("This application corresponds to an example in ",span("Mathematical Statistics and Data Analysis",style = "font-style:italic"), "section 3.5, example E, by John Rice."),
+              h5("The goal of the example is to update our belief about the parameter \\(\\Theta\\) = the probability of obtaining a head when a particular coin is flipped.  The experiment is to flip the coin 30 times and observe the number of heads. The likelihood is then a binomial distribution. The prior is assumed to be a Beta distribution."),
+              h4("For the 'Gamma & Poisson' app:"),
+              h5("The prior distribution is a Gamma and the likelihood is a Poisson."),
+              h5("This is a conjugate relationship so the posterior is also a Gamma.")
             )
           ),
           
@@ -47,8 +53,7 @@ dashboardPage(skin="red",
             h1("How to use the app?"),
             #box to contain description
             box(background="red",width=12,
-              h4("The controls for the app are located to the left and the visualizations are available on the right."),
-              h4("To change the number of successes observed (for example the number of coins landing head side up), the slider on the top left can be used."),
+              h4("The controls for the apps are located to the left and the visualizations are available on the right."),
               h4("To change the prior distribution, the hyperparameters can be set using the input boxes on the left.  The changes in this distribution can be seen on the first graph."),
               h4("The resulting changes to the posterior distribution can be seen on the second graph.")
             )
@@ -82,11 +87,46 @@ dashboardPage(skin="red",
               box(width=6,
                 plotOutput("distPlot"),
                 br(),
-                h4("Posterior distribution for the probability of success \\(\\Theta\\).")
+                h4("Posterior distribution for the probability of success \\(\\Theta|y\\).")
               )
             )
           )
         )
+      ),
+      tabItem(tabName = "app2",
+              fluidRow(
+                column(width=3,
+                       box(width=12,background="red",numericInput("sampleMean","Sample mean from data",min = 0,max = 1000, value = 1)
+                       ),
+                       box(width=12,background="red",numericInput("sampleSize","Sample size",min = 1,max = 1000, value = 10)
+                       ),
+                       box(width=12,
+                           title="Hyperparameters of the prior distribution for \\(\\Lambda\\)",
+                           background="red",
+                           solidHeader=TRUE,
+                           p("\\(\\frac{\\beta^\\alpha}{\\Gamma(\\beta)}\\lambda^{\\alpha-1}e^{-\\beta\\lambda}\\)"),
+                           h5("(Set to 1 if blank.)"),
+                           numericInput("alpha2",label=h5("\\(\\alpha\\) Value (> 0)"),value=1,min=0.0001,step=0.1),
+                           numericInput("beta2",label=h5("\\(\\beta\\) Value (> 0)"),value=1,min=0.0001,step=0.1)
+                       )
+                ),
+                column(width=9,
+                       fluidRow(
+                         box(width=6,
+                             plotOutput("priorPlot2"),
+                             br(),
+                             h4("Prior distribution for the rate parameter \\(\\Lambda\\).")
+                         ),
+                         box(width=6,
+                             plotOutput("distPlot2"),
+                             br(),
+                             h4("Posterior distribution for rate parameter \\(\\Lambda|y_1,...,y_n\\)."),
+                             h4("Theoretically, this posterior is given by"),
+                             h4("\\(\\frac{(\\beta+n)^{\\alpha+\\sum_{i=1}^{n}y_i}}{\\Gamma(\\beta+n)}\\lambda^{\\alpha+\\sum_{i=1}^{n}y_i-1}e^{-(\\beta+n)\\lambda}\\)")
+                         )
+                       )
+                )
+              )
       )
     )
   )
